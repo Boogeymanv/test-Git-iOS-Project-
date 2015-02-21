376b5809b8cdb35c596b6be80a4780ade30f8064
VIGNESH SHETTY <vigneshshetty@VIGNESHs-MacBook-Pro.local>
2015-02-21 23:06:05 +0530

    added a test class

---
+++ gitTest/CCTest.h
@@ -0,0 +1,13 @@
+//
+//  CCTest.h
+//  gitTest
+//
+//  Created by VIGNESH SHETTY on 21/02/15.
+//  Copyright (c) 2015 SeizeTechnologies. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface CCTest : NSObject
+
+@end
---
+++ gitTest/CCTest.m
@@ -0,0 +1,13 @@
+//
+//  CCTest.m
+//  gitTest
+//
+//  Created by VIGNESH SHETTY on 21/02/15.
+//  Copyright (c) 2015 SeizeTechnologies. All rights reserved.
+//
+
+#import "CCTest.h"
+
+@implementation CCTest
+
+@end
