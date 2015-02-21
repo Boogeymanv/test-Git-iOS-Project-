2dd8277240d3328730771880d9419bdb61b08686
VIGNESH SHETTY <vigneshshetty@VIGNESHs-MacBook-Pro.local>
2015-02-21 22:25:54 +0530

    Initial Commit

---
+++ gitTest/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  gitTest
+//
+//  Created by VIGNESH SHETTY on 21/02/15.
+//  Copyright (c) 2015 SeizeTechnologies. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ gitTest/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  gitTest
+//
+//  Created by VIGNESH SHETTY on 21/02/15.
+//  Copyright (c) 2015 SeizeTechnologies. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
